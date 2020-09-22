4352e0029b0482b77ac3570e98a8659e0ff0b209
parsonsrd <parsonsrd@gmail.com>
2020-09-22 14:04:26 +0200

    Added many more comments

--- corsika_toy_iact/iact_array.py
+++ corsika_toy_iact/iact_array.py
@@ -4,6 +4,11 @@ from scipy.ndimage import gaussian_filter
 from numpy.random import normal
 import uproot
 """
+This class is created to process to output from CORSIKA Cherenkov output files (after conversion to ROOT TTrees)
+This output can then be read and processed to simply simulate the behaviour of IACTs. This is done by simply binning 
+the directions of the photons landing on a region of the ground in a histogram. Then the photon positions can be 
+smoothed to represent the effect of the optical PSF, then efficiency factors and noise added. This effectively converts
+this number of photons to a number of photoelectrons as typically measured in a Cherenkov telescope.
 """
 
 
@@ -11,13 +16,17 @@ class IACTArray:
 
     def __init__(self, telescope_positions, radius=5, camera_size=4, bins=80):
         """
-
-        :param telescope_positions:
-        :param radius:
-        :param camera_size:
-        :param bins:
+        :param telescope_positions: ndarray
+            X, Y positions of telescopes of ground (N, 2)
+        :param radius: float
+            Radius of telescope mirrors (m)
+        :param camera_size: float
+            Size of camera (deg)
+        :param bins: int
+            Number of pixels on each camera axis
         """
 
+        # Just copy everything into this class that we need
         telescope_positions = np.array(telescope_positions)
         self.telescope_x_positions = telescope_positions.T[0]
         self.telescope_y_positions = telescope_positions.T[1]
@@ -32,35 +41,49 @@ class IACTArray:
     @staticmethod
     def _get_event_numbers(photon_list):
         """
+        Gets a list of unique event numbers in the file read it
 
-        :param photon_list:
-        :return:
+        :param photon_list: dict
+            Dictionary of the Cherenkov ROOT file contents
+        :return: ndarray
+            Array of unique event numbers
         """
         events = photon_list["event"]
         return np.unique(events)
 
     def process_images(self, photon_list):
         """
+        Read in the list of photons and create a binned camera image for all defined telescopes
+        and all events
 
-        :param photon_list:
-        :return:
+        :param photon_list: dict
+            Dictionary of the Cherenkov ROOT file contents
+        :return: ndarray
+            4D array of camera images for all events and telescopes
         """
 
+        # First get our list of unique event numbers
         event_nums = self._get_event_numbers(photon_list)
         image_list = []
 
         print('Processing images')
+        # Then loop over the events
         for event in tqdm(event_nums):
 
+            # Select the photons from the list belonging to this event
             selection = photon_list["event"] == event
+            # Photons positions in m
             x = photon_list["x"][selection]/100
             y = photon_list["y"][selection]/100
 
+            # Photons directions in deg
             u = np.rad2deg(photon_list["u"][selection])
             v = np.rad2deg(photon_list["v"][selection])
 
+            # Weight of each photon
             weights = photon_list["s"][selection]
 
+            # Calculate which telescope each photon belongs to
             r = np.sqrt(np.power(x[:, np.newaxis] - self.telescope_x_positions, 2) +
                         np.power(y[:, np.newaxis] - self.telescope_y_positions, 2))
             telescope_selection = np.array(r < self.telescope_radius, np.int)
@@ -69,14 +92,17 @@ class IACTArray:
 
             telescope_seen = np.sum(telescope_selection, axis=1)
 
+            # Then bin our photons
             content, edges = np.histogramdd((telescope_seen, u, v),
                                             bins=(self.num_telescope, self.camera_axis_bins, self.camera_axis_bins),
                                             range=((0.5, self.num_telescope+1.5),
                                                    (-self.camera_radius, self.camera_radius),
                                                    (-self.camera_radius, self.camera_radius)),
                                             weights=weights)
+            # Add this event onto our list of events
             image_list.append(content)
 
+        # And add ont our array for all files
         image_list = np.array(image_list)
         if self.images is None:
             self.images = image_list
@@ -87,22 +113,28 @@ class IACTArray:
 
     def process_file_list(self, file_list):
         """
+        Read in and bin photons from a list of CORSIKA output ROOT files
 
-        :param file_list:
-        :return:
+        :param file_list: list
+            List of files to read in
         """
 
         for file in file_list:
+            print('Reading', file)
+
             events = uproot.open(file)["photons"]
             branches = events.arrays(["event", "x", "y", "u", "v", "s"], namedecode="utf-8")
             self.process_images(branches)
 
     def _apply_optical_psf(self, images, psf_width, **kwargs):
         """
-
-        :param images:
-        :param psf_width:
-        :return:
+        Smooth the images with the a gaussian kernel to represent the PSF of the telescope
+        :param images: ndarray
+            Array of camera images
+        :param psf_width: float
+            Width of gaussian PSF
+        :return: ndarray
+            Smoothed camera images
         """
         psf_width = psf_width * (self.camera_axis_bins / (self.camera_radius * 2))
         return gaussian_filter(images, sigma=(0, 0, psf_width, psf_width))
@@ -110,11 +142,17 @@ class IACTArray:
     @staticmethod
     def _apply_efficiency(images, mirror_reflectivity=0.8, quantum_efficiency=0.2, pedestal_width=1, **kwargs):
         """
+        Apply scaling factor to simulate the efficiency of the mirrors and photodetectors. Add random gaussian
+        pedestal values
 
-        :param images:
-        :param mirror_reflectivity:
-        :param quantum_efficiency:
-        :param pedestal_width:
+        :param images: ndarray
+            Array of camera images
+        :param mirror_reflectivity: float
+            Efficiency of mirrors
+        :param quantum_efficiency: float
+            Efficiency of photodetectors
+        :param pedestal_width: float
+            Width of gaussian pedestal to add
         :return:
         """
         scaled_images = images * mirror_reflectivity * quantum_efficiency
@@ -123,8 +161,9 @@ class IACTArray:
 
     def scale_to_photoelectrons(self, **kwargs):
         """
-
-        :return:
+        Apply both efficiency smoothing in one step
+        :return: ndarray
+            Images now converted to photoelectrons
         """
         smoothed_images = self._apply_optical_psf(self.images, **kwargs)
         return self._apply_efficiency(smoothed_images, **kwargs)
--- examples/analyse_example.py
+++ examples/analyse_example.py
@@ -1,7 +1,9 @@
 import uproot
 import corsika_toy_iact.iact_array as iact
+import numpy as np
 
-iact_array = iact.IACTArray([[0.,0.], [20.,0.]])
+positions = np.array([[0, 0], [0, 20], [0, 40], [0, 60]])
+iact_array = iact.IACTArray(positions)
 image = iact_array.process_file_list(["data/epos_1TeV_cher.root"])
 
 image_pe = iact_array.scale_to_photoelectrons(psf_width=0.05, mirror_reflectivity=0.8, pedestal_width=0.8)
