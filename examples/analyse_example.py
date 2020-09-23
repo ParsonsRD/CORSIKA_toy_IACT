7509157ed6c1341cd32df24eef0a1854b58c12f3
parsonsrd <parsonsrd@gmail.com>
2020-09-23 15:48:05 +0200

    Example file updated to get example dataset

--- examples/analyse_example.py
+++ examples/analyse_example.py
@@ -1,14 +1,17 @@
 import uproot
 import corsika_toy_iact.iact_array as iact
 import numpy as np
+import pkg_resources
+import matplotlib.pyplot as plt
 
 positions = np.array([[0, 0], [0, 20], [0, 40], [0, 60]])
 iact_array = iact.IACTArray(positions)
-image = iact_array.process_file_list(["data/epos_10TeV_cher.root"])
+data_dir = pkg_resources.resource_filename('corsika_toy_iact', 'data/')
+
+image = iact_array.process_file_list([data_dir+"test_data.root"])
 
 image_pe = iact_array.scale_to_photoelectrons(psf_width=0.05, mirror_reflectivity=0.8, pedestal_width=0.8)
 
-import matplotlib.pyplot as plt
 for image in image_pe:
     plt.imshow(image[0])
     plt.colorbar()
