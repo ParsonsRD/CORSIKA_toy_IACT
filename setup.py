2ed03d529faec930a49c60ff7d7f20ebb825c91e
parsonsrd <parsonsrd@gmail.com>
2020-09-23 15:46:52 +0200

    Fix setup instructions

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='corsika_toy_iact',
-    version='1.0',
-    packages=['corsika_toy_iact'],
+    version='1.1',
+    packages=find_packages(include=['corsika_toy_iact', 'corsika_toy_iact.*']),
     package_dir={'corsika_toy_iact': 'corsika_toy_iact'},
     package_data={'corsika_toy_iact': ["data/test_data.root"]},
     include_package_data=True,
     url='',
     license='',
-    author='dparsons',
-    author_email='',
-    description='Simple code for reproducing behaviour of Cherenkov telescopes'
+    zip_safe=False,
+    author='parsonsrd',
+    description='Simple code for reproducing behaviour of Cherenkov telescopes',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
