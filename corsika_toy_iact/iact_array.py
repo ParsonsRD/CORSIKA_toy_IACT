1ca13fcddae37074ffdef43591637d3b2f6b4e5c
parsonsrd <parsonsrd@gmail.com>
2020-11-06 12:01:11 +0100

    Fixes to allow inclusion of multiple core reuses

--- corsika_toy_iact/iact_array.py
+++ corsika_toy_iact/iact_array.py
@@ -3,6 +3,8 @@ from tqdm import tqdm
 from scipy.ndimage import gaussian_filter
 from numpy.random import normal, poisson
 import uproot4 as uproot
+from ctapipe.coordinates import AltAz, NominalFrame
+import astropy.units as units
 
 """
 This class is created to process to output from CORSIKA Cherenkov output files (after conversion to ROOT TTrees)
@@ -15,7 +17,7 @@ this number of photons to a number of photoelectrons as typically measured in a 
 
 class IACTArray:
 
-    def __init__(self, telescope_positions, radius=5, camera_size=4, bins=80):
+    def __init__(self, telescope_positions, radius=5, camera_size=8, bins=40):
         """
         :param telescope_positions: ndarray
             X, Y positions of telescopes of ground (N, 2)
@@ -31,7 +33,6 @@ class IACTArray:
         telescope_positions = np.array(telescope_positions)
         self.telescope_x_positions = telescope_positions.T[0]
         self.telescope_y_positions = telescope_positions.T[1]
-        print(self.telescope_x_positions)
 
         self.telescope_radius = radius
         self.camera_radius = camera_size/2.
@@ -53,7 +54,7 @@ class IACTArray:
         events = photon_list["event"]
         return np.unique(events)
 
-    def process_images(self, photon_list):
+    def process_images(self, header, photon_list):
         """
         Read in the list of photons and create a binned camera image for all defined telescopes
         and all events
@@ -65,34 +66,44 @@ class IACTArray:
         """
 
         # First get our list of unique event numbers
-        event_nums = self._get_event_numbers(photon_list)
+        event_nums = header["event"]#self._get_event_numbers(header)
         image_list = []
+        event_count = 0
+        array_pointing = AltAz(alt=90*units.deg, az=0*units.deg)
 
         print('Processing images')
+        previous_event = -1
         # Then loop over the events
         for event in tqdm(event_nums):
 
+            event_base = int(np.floor(event/100.))
             # Select the photons from the list belonging to this event
-            selection = photon_list["event"] == event
-            # Photons positions in m
-            x = photon_list["x"][selection]/100
-            y = photon_list["y"][selection]/100
+            selection = photon_list["event"] == event_base
 
-            # Photons directions in deg
-            u = np.rad2deg(photon_list["u"][selection])
-            v = np.rad2deg(photon_list["v"][selection])
+            if event_base != previous_event:
+                # Photons positions in m
+                x = photon_list["x"][selection]/100
+                y = photon_list["y"][selection]/100
 
-            # Weight of each photon
-            weights = photon_list["s"][selection]
+                # Photons directions in deg
+                u = photon_list["u"][selection]
+                v = photon_list["v"][selection]
+
+                # Weight of each photon
+                weights = photon_list["s"][selection]
+                previous_event = event_base
 
             # Calculate which telescope each photon belongs to
-            r = np.sqrt(np.power(x[:, np.newaxis] - self.telescope_x_positions, 2) +
-                        np.power(y[:, np.newaxis] - self.telescope_y_positions, 2))
+            r = np.sqrt(np.power(x[:, np.newaxis] - (self.telescope_x_positions + header["core_x"][event_count]), 2) +
+                        np.power(y[:, np.newaxis] - (self.telescope_y_positions + header["core_y"][event_count]), 2))
+
             telescope_selection = np.array(r < self.telescope_radius, np.int)
             tel_list = np.arange(1, self.num_telescope+1)
             telescope_selection *= tel_list[np.newaxis, :]
 
             telescope_seen = np.sum(telescope_selection, axis=1)
+
+
             # Then bin our photons
             content, edges = np.histogramdd((telescope_seen, u, v),
                                             bins=(self.num_telescope, self.camera_axis_bins, self.camera_axis_bins),
@@ -102,6 +113,7 @@ class IACTArray:
                                             weights=weights)
             # Add this event onto our list of events
             image_list.append(content)
+            event_count = event_count + 1
 
         # And add ont our array for all files
         image_list = np.array(image_list)
@@ -112,7 +124,7 @@ class IACTArray:
 
         return image_list
 
-    def process_file_list(self, file_list):
+    def process_file_list(self, file_list, max_events=1e10):
         """
         Read in and bin photons from a list of CORSIKA output ROOT files
 
@@ -120,15 +132,22 @@ class IACTArray:
             List of files to read in
         """
         self.reset()
-
+        header_array = None
         for file in file_list:
             print('Reading', file)
 
             events = uproot.open(file)
-            branches = events["photons"].arrays(library="np")
-            self.process_images(branches)
 
-        return self.images
+            branches = events["photons"].arrays(library="np")
+            header = events["header"].arrays(library="np", entry_stop=max_events)
+
+            if header_array is None:
+                header_array = header
+            else:
+                header_array = np.concatenate(header_array, header, axis=0)
+            self.process_images(header, branches)
+
+        return header_array, self.images.astype(np.float32)
 
     def _apply_optical_psf(self, images, psf_width, **kwargs):
         """
@@ -163,9 +182,24 @@ class IACTArray:
         scaled_images = images * mirror_reflectivity * quantum_efficiency
         pedestal = normal(0, pedestal_width, images.shape)
 
-        #scaled_images = np.round(scaled_images)
+        # Should we round here for Poisson?
         scaled_images = normal(scaled_images, np.sqrt(scaled_images) * single_pe_width)
         return scaled_images + pedestal
+
+    @staticmethod
+    def _apply_photon_cut(images, photon_cut=10, **kwargs):
+        """
+
+        :param images:
+        :param photon_cut:
+        :param kwargs:
+        :return:
+        """
+        flatten_image = images.reshape(images.shape[0], images.shape[1], images.shape[2] * images.shape[3])
+        image_sum = np.sum(flatten_image, axis=-1)
+
+        image_mask = image_sum > photon_cut
+        return image_mask[:, :, np.newaxis, np.newaxis]
 
     def scale_to_photoelectrons(self, **kwargs):
         """
@@ -173,8 +207,9 @@ class IACTArray:
         :return: ndarray
             Images now converted to photoelectrons
         """
+
         smoothed_images = self._apply_optical_psf(self.images, **kwargs)
-        return self._apply_efficiency(smoothed_images, **kwargs)
+        return self._apply_efficiency(smoothed_images, **kwargs) * self._apply_photon_cut(self.images, **kwargs)
 
     def get_camera_geometry(self):
         """
@@ -184,9 +219,11 @@ class IACTArray:
             Camera geometry object
         """
         from ctapipe.instrument import CameraGeometry
+        half_pixel_size = self.camera_radius/float(self.camera_axis_bins)
+        radius = self.camera_radius-half_pixel_size
         return CameraGeometry.make_rectangular(self.camera_axis_bins, self.camera_axis_bins,
-                                               range_x=(-1*self.camera_radius,  self.camera_radius),
-                                               range_y=(-1*self.camera_radius,  self.camera_radius))
+                                               range_x=(-radius, radius),
+                                               range_y=(-radius,  radius))
 
     def reset(self):
         self.images = None
--- examples/analyse_example.py
+++ examples/analyse_example.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 from ctapipe.image import tailcuts_clean
 from ctapipe.image.hillas import hillas_parameters, HillasParameterizationError
 
-positions = np.array([[0, 0], [20, 0], [40, 0], [200, 0]])
-iact_array = iact.IACTArray(positions)
+x = np.linspace(-180, 180, 4)
+xx, yy = np.array(np.meshgrid(x, x))
+positions = np.vstack((xx.ravel(), yy.ravel())).T
+
+iact_array = iact.IACTArray(positions, radius=6)
 data_dir = pkg_resources.resource_filename('corsika_toy_iact', 'data/')
 
 #image = iact_array.process_file_list([data_dir+"test_data.root"])
-image = iact_array.process_file_list(["corsika_toy_iact/data/epos_10TeV_cher.root"])
+header, image = iact_array.process_file_list(["corsika_toy_iact/data/CER000001.root"])
 
 image_pe = iact_array.scale_to_photoelectrons(psf_width=0.04, mirror_reflectivity=0.8,
                                               quantum_efficiency=0.2, pedestal_width=0.8,
-                                              single_pe_width=0.5)
+                                              single_pe_width=0.5, photon_cut=10)
 
 geometry = iact_array.get_camera_geometry()
 i, j = 0, 0
@@ -24,7 +27,7 @@ width = np.zeros((image_pe.shape[0], image_pe.shape[1]))
 for event in image_pe:
     for image in event:
         mask = tailcuts_clean(geometry, image.ravel(),
-                              picture_thresh=8, boundary_thresh=4).reshape(80, 80)
+                              picture_thresh=8, boundary_thresh=4).reshape(40, 40)
         image[np.invert(mask)] = 0
         try:
             hill = hillas_parameters(geometry, image.ravel())
@@ -36,5 +39,7 @@ for event in image_pe:
     i += 1
     j = 0
 
-plt.hist(width.T[3], bins=50)
+print(np.sum(header["energy"] > 10000), np.sum(width[header["energy"] > 10000].ravel() > 0))
+plt.hist(width[header["energy"] > 10000].ravel(), bins=50, log=True)
+
 plt.show()
